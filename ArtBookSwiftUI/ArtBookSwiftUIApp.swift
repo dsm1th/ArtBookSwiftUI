edce0f935b7ede470431fe6ae036ddc566b7688a
Darren Smith <dsm1th@mac.com>
2024-08-18 13:27:30 +0100

    Initial Commit

---
+++ ArtBookSwiftUI/ArtBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtBookSwiftUIApp.swift
+//  ArtBookSwiftUI
+//
+//  Created by Darren Smith on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ArtBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtBookSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtBookSwiftUI
+//
+//  Created by Darren Smith on 18/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
