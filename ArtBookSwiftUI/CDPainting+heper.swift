f531f7146ea861d068592be3c81032bddab71efc
Darren Smith <dsm1th@mac.com>
2024-08-24 23:03:17 +0100

    sorted CDPaintings. Still working on filtering and ViewModel integration.

---
+++ ArtBookSwiftUI/CDPainting+heper.swift
@@ -0,0 +1,8 @@
+//
+//  CDPainting+heper.swift
+//  ArtBookSwiftUI
+//
+//  Created by Darren Smith on 21/08/2024.
+//
+
+import Foundation
---
+++ ArtBookSwiftUI/NSPredicate+helper.swift
@@ -0,0 +1,8 @@
+//
+//  NSPredicate+helper.swift
+//  ArtBookSwiftUI
+//
+//  Created by Darren Smith on 22/08/2024.
+//
+
+import Foundation
